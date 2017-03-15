b167bc86ef8a8826d78b9e94a8cba5b0036260ea
DonNinja <yngvileo@gmail.com>
2017-03-15 09:58:47 +0000

    Byrjaður á nokkru, setti inn takka og byrjaði að vinna á símasíðunni.

--- index.php
+++ index.php
@@ -2,12 +2,39 @@
 <html>
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="styles/main.css">
+	<link rel="icon" href="http://www.gamescom-cologne.com/redaktionell/gamescom_1/img_32/layout_25/gamescom_favicon_32x32.jpg">
 	<title>Skilaverkefni 4</title>
 </head>
 	<body>
+	<div class="contallt">
+		<h1 class="tit">gamescom 2017</h1>
 		<div class="cont">
-			bgnhh
+			<div class="cont3">
+				<span class="tital">So what is gamescom?</span>
+				<p>Gamescom is the world's largest gaming event, (measured by exhibition space and number of visitors) with 345,000 visitors, more than 6,000 journalists and 700 exhibitors from 88 countries attending the show in its sixth year.</p>
+			</div>
+			<div class="cont3">
+				 <span class="tital">On the current line-up we have</span>
+				 <ul>
+				 	<li>A conference by Bethesda (08:15)</li>
+				 	<li>A show put on by Nintendo (09:00)</li>
+				 	<li>Microsoft will show their new console (10:30)</li>
+				 </ul>
+				 And more, but we don't want to spoil the show, there will be short breaks between each event so you can walk around the booths and check everything out.
+			</div>
+			<input type="checkbox" id="toggle">
+			<label for="toggle">&#9776;</label>
+			<nav class="cont3 cont4">
+				<div class="navv">
+				<nav class="navi navi-1 navi-y">Verkefni 5</nav>
+				<nav class="navi navi-2 navi-y">Verkefni 5</nav>
+				<nav class="navi navi-3 navi-y">Verkefni 5</nav>
+				<nav class="navi navi-4 navi-y">Verkefni 5</nav>
+				</div>
+			</nav>
 		</div>
+	</div>
 	</body>
 </html>
